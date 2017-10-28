e5fc711c55552f162ab84c452f3e1f255fcaf4c8
caioluke <31912369+caioluke@users.noreply.github.com>
2017-10-28 14:45:33 -0200

    hummmmmmm

---
+++ H10/bash.sh
@@ -0,0 +1,40 @@
+cd teta16
+gfortran flow_flowLT16.f90
+mv a.out T16.out
+nohup ./T16.out &
+cd ..
+cd teta18
+gfortran flow_flowLT18.f90
+mv a.out T18.out
+nohup ./T18.out &
+cd ..
+cd teta19
+gfortran flow_flowLT19.f90
+mv a.out T19.out
+nohup ./T19.out &
+cd ..
+cd teta20
+gfortran flow_flowLT20.f90
+mv a.out T20.out
+nohup ./T20.out &
+cd ..
+cd teta23
+gfortran flow_flowLT23.f90
+mv a.out T23.out
+nohup ./T23.out &
+cd ..
+cd teta24
+gfortran flow_flowLT24.f90
+mv a.out T24.out
+nohup ./T24.out &
+cd ..
+cd teta25
+gfortran flow_flowLT25.f90
+mv a.out T25.out
+nohup ./T25.out &
+cd ..
+cd teta26
+gfortran flow_flowLT26.f90
+mv a.out T26.out
+nohup ./T26.out &
+cd ..

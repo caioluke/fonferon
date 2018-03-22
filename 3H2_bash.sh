762977a1aab0c8b47b608b3ad49212014e2e39c3
caioluke <31912369+caioluke@users.noreply.github.com>
2018-03-22 10:22:09 -0300

    q

--- 3H2_bash.sh
+++ 3H2_bash.sh
@@ -1,22 +1,4 @@
 cd H30
-cd 12teta
-gfortran -O 12teta.f90
-mv a.out T12.out
-nohup ./T12.out &
-cd ..
-
-cd 14teta
-gfortran -O 14teta.f90
-mv a.out T14.out
-nohup ./T14.out &
-cd ..
-
-cd 16teta
-gfortran -O 16teta.f90
-mv a.out T16.out
-nohup ./T16.out &
-cd ..
-
 cd 20teta
 gfortran -O 20teta.f90
 mv a.out T20.out

f009467b78d2f9d0233d724cc224229adc6d2247
caioluke <31912369+caioluke@users.noreply.github.com>
2018-03-21 23:09:18 -0300

    opa

--- 30H_bash.sh
+++ 30H_bash.sh
@@ -35,15 +35,3 @@ mv a.out T24.out
 nohup ./T24.out &
 cd ..
 cd ..
-
-cd H40
-gfortran -O H40_posinit.f90
-mv a.out H40.out
-nohup ./H40.out &
-cd ..
-
-cd H50
-gfortran -O H50_posinit.f90
-mv a.out H50.out
-nohup ./H50.out &
-cd ..

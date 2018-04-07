14f09b7108685b3abc032c1272a61f95fac1bddb
caioluke <31912369+caioluke@users.noreply.github.com>
2018-04-06 22:19:12 -0300

    agr vai

---
+++ H40/teste.sh
@@ -0,0 +1,29 @@
+cp 01_flow.f90 12teta.f90
+mkdir 12teta
+mv 12teta.f90 12teta/
+cp initflow.dat 12teta/initflow.dat
+
+cp 01_flow.f90 14teta.f90
+mkdir 14teta
+mv 14teta.f90 14teta/
+cp initflow.dat 14teta/initflow.dat
+
+cp 01_flow.f90 16teta.f90
+mkdir 16teta
+mv 16teta.f90 16teta/
+cp initflow.dat 16teta/initflow.dat
+
+cp 01_flow.f90 20teta.f90
+mkdir 20teta
+mv 20teta.f90 20teta/
+cp initflow.dat 20teta/initflow.dat
+
+cp 01_flow.f90 22teta.f90
+mkdir 22teta
+mv 22teta.f90 22teta/
+cp initflow.dat 22teta/initflow.dat
+
+cp 01_flow.f90 24teta.f90
+mkdir 24teta
+mv 24teta.f90 24teta/
+cp initflow.dat 24teta/initflow.dat
---
+++ bashGIT.sh
@@ -0,0 +1,16 @@
+cd H30
+git add 12teta/
+ls
+git add 14teta/
+ls
+git add 16teta/
+ls
+git add 20teta/
+ls
+git add 22teta/
+ls
+git add 24teta/
+ls
+cd ..
+git add .
+git commit -m "la vie est belle"
